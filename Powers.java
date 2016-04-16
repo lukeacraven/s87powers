0b95bb7e388caad37d1c59b8340f351dd6519be6
RamanujansGhost <lukecraven1996@gmail.com>
2016-04-16 02:19:44 -0400

    Added files via upload

---
+++ Powers.java
@@ -0,0 +1,107 @@
+package io.github.ramanujansghost;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Powers extends JavaPlugin
+{
+	@Override
+    public void onEnable()
+    {
+
+    }
+	
+    @Override
+    public void onDisable()
+    {
+    	getLogger().info("Powers has been disabled!");
+    }
+    
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+    {
+    	if(cmd.getName().equalsIgnoreCase("powers"))
+    	{
+    		if(args.length == 0)
+    		{
+    			sender.sendMessage(ChatColor.DARK_AQUA + "Powers v.01");
+    			sender.sendMessage(ChatColor.GOLD + "/powers - This command");
+    			sender.sendMessage(ChatColor.GOLD + "/powers list - Lists the powers");
+    			sender.sendMessage(ChatColor.GOLD + "/powers lookup <playername> - Looks up <playername>'s");
+    			sender.sendMessage(ChatColor.GOLD + "/powers add <power> <playername> - Gives <power> to <playername>");
+    			sender.sendMessage(ChatColor.GOLD + "/powers remove <power> <playername> - Removes <power> from <playername>");
+    		}
+    		else if(args.length >= 1)
+    		{
+    			if(args[0].equalsIgnoreCase("add"))
+    			{
+    				if(args.length == 3)
+    				{
+    					if(sender.isOp())
+    					{
+    						sender.sendMessage("Adding power " + args[1] + " to " + args[2]);
+    					}
+    					else
+    					{
+    						sender.sendMessage("Error while performing command!  User is not opped");
+    					}
+    				}
+    				else
+    				{
+    					sender.sendMessage("Error while performing command!  Syntax is /powers add <power> <playername>");
+    				}
+    			}
+    			if(args[0].equalsIgnoreCase("help"))
+    			{
+        			sender.sendMessage(ChatColor.GOLD + "/powers - Lists these help commands and the version number");
+        			sender.sendMessage(ChatColor.GOLD + "/powers list - Lists the powers");
+        			sender.sendMessage(ChatColor.GOLD + "/powers lookup <playername> - Looks up <playername>'s");
+        			sender.sendMessage(ChatColor.GOLD + "/powers add <power> <playername> - Gives <power> to <playername>");
+        			sender.sendMessage(ChatColor.GOLD + "/powers remove <power> <playername> - Removes <power> from <playername>");
+    			}
+    			if(args[0].equalsIgnoreCase("list"))
+    			{
+    				sender.sendMessage("The powers are as follows: ");
+    			}
+    			if(args[0].equalsIgnoreCase("lookup"))
+    			{
+    				if(args.length == 2)
+    				{
+    					if(sender.isOp())
+    					{
+    						sender.sendMessage("Looking up " + args[1] + "'s powers");
+    					}
+    					else
+    					{
+    						sender.sendMessage("Error while performing command!  User is not opped");
+    					}
+    				}
+    				else
+    				{
+    					sender.sendMessage("Error while performing command!  Syntax is /powers lookup <playername>");
+    				}
+    			}
+    			if(args[0].equalsIgnoreCase("remove"))
+    			{
+    				if(args.length == 3)
+    				{
+    					if(sender.isOp())
+    					{
+    						sender.sendMessage("Removing power " + args[1] + " from " + args[2]);
+    					}
+    					else
+    					{
+    						sender.sendMessage("Error while performing command!  User is not opped");
+    					}
+    				}
+    				else
+    				{
+    					sender.sendMessage("Error while performing command!  Syntax is /powers remove <power> <playername>");
+    				}
+    			}
+    		}
+    	}	
+        return false;
+    }
+}
