a2e2bd95e0bef38636f833b208208fc680e41148
RamanujansGhost <lukecraven1996@gmail.com>
2016-04-19 16:45:47 -0400

    Finished polishing Lumberjack code -- it is fully finished now unless a cost needs to be added

--- io/github/ramanujansghost/s87powers/Lumberjack.java
+++ io/github/ramanujansghost/s87powers/Lumberjack.java
@@ -1,24 +1,22 @@
 package io.github.ramanujansghost.s87powers;
 
-import com.massivecraft.factions.entity.BoardColl;
-import com.massivecraft.massivecore.ps.PS;
-
 import java.util.logging.Level;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.player.PlayerItemDamageEvent;
+import org.bukkit.inventory.ItemStack;
 
-
-//make Lumberjack respect Factions territories
 public class Lumberjack extends Power
 {
 	private static short blocksBroken = 0;
-	private final int diamondAxeDurability = 1561;
+	private static final int goldAxeDurability = 33;
+	private static final int woodAxeDurability = 60;
+	private static final int stoneAxeDurability = 132;
+	private static final int ironAxeDurability = 251;
+	private static final int diamondAxeDurability = 1562;
 	
 	@Override
 	public String getName()
@@ -43,46 +41,69 @@ public class Lumberjack extends Power
 		short initialDurability = 0;
 		short damageCaused = 0;
 		
-		if(event.getPlayer().getEquipment().getItemInMainHand() != null)
-		{
-			initialDurability = event.getPlayer().getEquipment().getItemInMainHand().getDurability();
-		}
-		else
-		{
-			S87Powers.log.log(Level.WARNING, event.getPlayer() + "'s axe is missing..  perhaps it broke? (Was checking durability)");
-			return;
-		}
-			
-		blocksBroken = 0;
-		breakAdjacentBlocks(event.getPlayer(), event.getBlock());
-		int durabilityEnchantment = event.getPlayer().getEquipment().getItemInMainHand().getEnchantmentLevel(Enchantment.DURABILITY);
-		S87Powers.log.log(Level.INFO, "Durability enchantment level: " + durabilityEnchantment);
-		damageCaused = (short) (blocksBroken + 1);
+		Player p = event.getPlayer();
+		ItemStack axeItem = p.getEquipment().getItemInMainHand();
+		Material axeType = axeItem.getType();
 		
-		if(S87Powers.debugPowers)
+		initialDurability = axeItem.getDurability();
+		blocksBroken = 0;
+		
+		breakAdjacentBlocks(p, event.getBlock());
+		
+		int levelOfDurabilityEnchantment = axeItem.getEnchantmentLevel(Enchantment.DURABILITY);
+		//damageCaused uses the official minecraft formula to calculate damage based on durability enchant
+		if(levelOfDurabilityEnchantment != 0)
+			damageCaused = (short) (blocksBroken/(axeItem.getEnchantmentLevel(Enchantment.DURABILITY)+1));
+		else
+			damageCaused = blocksBroken;
+		
+		if(S87Powers.debugLumberjack)
 		{
 			S87Powers.log.log(Level.INFO, "Initial durability: " + initialDurability);
-			S87Powers.log.log(Level.INFO, "Damage caused: " + damageCaused);
+			S87Powers.log.log(Level.INFO, "Damage caused: " + damageCaused + " from breaking " + blocksBroken + " blocks");
 		}
 
-		/*
-		if(event.getPlayer().getEquipment().getItemInMainHand() != null)
+		axeItem.setDurability((short) (initialDurability + damageCaused));
+		short axeDurability = axeItem.getDurability();
+		axeItem = p.getEquipment().getItemInMainHand();		//update axeItem because we changed it by modifying durability
+			
+		if(axeType  == Material.GOLD_AXE)
 		{
-			event.getPlayer().getEquipment().getItemInMainHand().setDurability((short) (initialDurability + damageCaused));
+			if(axeDurability > goldAxeDurability)
+				InventoryHelper.breakTool(p, axeItem);
 		}
-		else
+					
+		else if(axeType  == Material.WOOD_AXE)
 		{
-			S87Powers.log.log(Level.WARNING, event.getPlayer() + "'s axe is missing..  perhaps it broke? (Was setting durability)");
-			return;
+			if(axeDurability > woodAxeDurability)
+				InventoryHelper.breakTool(p, axeItem);
 		}
-		*/
+
+		else if(axeType  == Material.STONE_AXE)
+		{
+			if(axeDurability > stoneAxeDurability)
+				InventoryHelper.breakTool(p, axeItem);
+		}
+			
+		else if(axeType  == Material.IRON_AXE)
+		{
+			if(axeDurability > ironAxeDurability)
+				InventoryHelper.breakTool(p, axeItem);
+		}
+			
+		else if(axeType  == Material.DIAMOND_AXE)
+		{
+			if(axeDurability > diamondAxeDurability)
+				InventoryHelper.breakTool(p, axeItem);
+		}	
 	}
 	
 	//make breakAdjacentBlocks method damage player's axe
 	public static void breakAdjacentBlocks(Player p, Block block)	//use a for loop here and check a 3x3 cube centered on the given block, but do not check center block.
 	{
-		if(S87Powers.debugPowers)
-			S87Powers.log.log(Level.INFO, "Received block " + block.getX() + ", " + block.getY() + ", " + block.getZ());
+		if(S87Powers.debugLumberjack)
+			S87Powers.log.log(Level.INFO, "Received block at " + block.getX() + ", " + block.getY() + ", " + block.getZ() + " checking around block.");
+		
 		for(int x = -1; x < 2; x++)
 		{
 			for(int y = -1; y < 2; y++)
