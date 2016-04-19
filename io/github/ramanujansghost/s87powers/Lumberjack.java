c887a0363bfe3134f850441aa76e7e6d1765b714
RamanujansGhost <lukecraven1996@gmail.com>
2016-04-19 14:55:26 -0400

    Cleaned up lumberjack code significantly.  Now uses a for loop.

--- io/github/ramanujansghost/s87powers/Lumberjack.java
+++ io/github/ramanujansghost/s87powers/Lumberjack.java
@@ -8,6 +8,7 @@ import java.util.logging.Level;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerItemDamageEvent;
@@ -17,6 +18,7 @@ import org.bukkit.event.player.PlayerItemDamageEvent;
 public class Lumberjack extends Power
 {
 	private static short blocksBroken = 0;
+	private final int diamondAxeDurability = 1561;
 	
 	@Override
 	public String getName()
@@ -53,6 +55,8 @@ public class Lumberjack extends Power
 			
 		blocksBroken = 0;
 		breakAdjacentBlocks(event.getPlayer(), event.getBlock());
+		int durabilityEnchantment = event.getPlayer().getEquipment().getItemInMainHand().getEnchantmentLevel(Enchantment.DURABILITY);
+		S87Powers.log.log(Level.INFO, "Durability enchantment level: " + durabilityEnchantment);
 		damageCaused = (short) (blocksBroken + 1);
 		
 		if(S87Powers.debugPowers)
@@ -60,12 +64,8 @@ public class Lumberjack extends Power
 			S87Powers.log.log(Level.INFO, "Initial durability: " + initialDurability);
 			S87Powers.log.log(Level.INFO, "Damage caused: " + damageCaused);
 		}
+
 		/*
-		Bukkit.getServer().getPluginManager().callEvent(new PlayerItemDamageEvent(event.getPlayer()
-				, event.getPlayer().getEquipment().getItemInMainHand()
-				, 1));
-		*/
-		
 		if(event.getPlayer().getEquipment().getItemInMainHand() != null)
 		{
 			event.getPlayer().getEquipment().getItemInMainHand().setDurability((short) (initialDurability + damageCaused));
@@ -74,7 +74,8 @@ public class Lumberjack extends Power
 		{
 			S87Powers.log.log(Level.WARNING, event.getPlayer() + "'s axe is missing..  perhaps it broke? (Was setting durability)");
 			return;
-		}	
+		}
+		*/
 	}
 	
 	//make breakAdjacentBlocks method damage player's axe
@@ -82,134 +83,26 @@ public class Lumberjack extends Power
 	{
 		if(S87Powers.debugPowers)
 			S87Powers.log.log(Level.INFO, "Received block " + block.getX() + ", " + block.getY() + ", " + block.getZ());
-			
-		//check +1y
-		if(block.getLocation().add(0,1,0).getBlock().getType() == Material.LOG 
-				|| block.getLocation().add(0,1,0).getBlock().getType() == Material.LOG_2 
-				|| block.getLocation().add(0,1,0).getBlock().getType() == Material.LEAVES
-				|| block.getLocation().add(0,1,0).getBlock().getType() == Material.LEAVES_2)
+		for(int x = -1; x < 2; x++)
 		{
-			if(S87Powers.canPlayerBuildAt(p, block.getLocation().add(0,1,0), block.getLocation().add(0,1,0).getBlock()))
+			for(int y = -1; y < 2; y++)
 			{
-				block.getLocation().add(0,1,0).getBlock().breakNaturally();
-				blocksBroken++;
-				breakAdjacentBlocks(p,block.getLocation().add(0,1,0).getBlock());
-			}	
-		}
-		//check -1y
-		if(block.getLocation().subtract(0,1,0).getBlock().getType() == Material.LOG 
-				|| block.getLocation().subtract(0,1,0).getBlock().getType() == Material.LOG_2 
-				|| block.getLocation().subtract(0,1,0).getBlock().getType() == Material.LEAVES
-				|| block.getLocation().subtract(0,1,0).getBlock().getType() == Material.LEAVES_2)
-		{
-			if(S87Powers.canPlayerBuildAt(p, block.getLocation().subtract(0,1,0), block.getLocation().subtract(0,1,0).getBlock()))
-			{
-				blocksBroken++;
-				breakAdjacentBlocks(p,block.getLocation().subtract(0,1,0).getBlock());
-			}	
-		}
-		//check +1x
-		if(block.getLocation().add(1,0,0).getBlock().getType() == Material.LOG 
-				|| block.getLocation().add(1,0,0).getBlock().getType() == Material.LOG_2 
-				|| block.getLocation().add(1,0,0).getBlock().getType() == Material.LEAVES
-				|| block.getLocation().add(1,0,0).getBlock().getType() == Material.LEAVES_2)
-		{
-			if(S87Powers.canPlayerBuildAt(p, block.getLocation().add(1,0,0), block.getLocation().add(1,0,0).getBlock()))
-			{
-				block.getLocation().add(1,0,0).getBlock().breakNaturally();
-				blocksBroken++;
-				breakAdjacentBlocks(p,block.getLocation().add(1,0,0).getBlock());
-			}
-		}
-		//check -1x
-		if(block.getLocation().subtract(1,0,0).getBlock().getType() == Material.LOG 
-				|| block.getLocation().subtract(1,0,0).getBlock().getType() == Material.LOG_2 
-				|| block.getLocation().subtract(1,0,0).getBlock().getType() == Material.LEAVES
-				|| block.getLocation().subtract(1,0,0).getBlock().getType() == Material.LEAVES_2)
-		{
-			if(S87Powers.canPlayerBuildAt(p, block.getLocation().subtract(1,0,0), block.getLocation().subtract(1,0,0).getBlock()))
-			{
-				block.getLocation().subtract(1,0,0).getBlock().breakNaturally();
-				blocksBroken++;
-				breakAdjacentBlocks(p,block.getLocation().subtract(1,0,0).getBlock());
-			}	
-		}
-		//check +1z
-		if(block.getLocation().add(0,0,1).getBlock().getType() == Material.LOG 
-				|| block.getLocation().add(0,0,1).getBlock().getType() == Material.LOG_2 
-				|| block.getLocation().add(0,0,1).getBlock().getType() == Material.LEAVES
-				|| block.getLocation().add(0,0,1).getBlock().getType() == Material.LEAVES_2)
-		{
-			if(S87Powers.canPlayerBuildAt(p, block.getLocation().add(0,0,1), block.getLocation().add(0,0,1).getBlock()))
-			{
-				block.getLocation().add(0,0,1).getBlock().breakNaturally();
-				blocksBroken++;
-				breakAdjacentBlocks(p, block.getLocation().add(0,0,1).getBlock());
-			}	
-		}
-		//check -1z
-		if(block.getLocation().subtract(0,0,1).getBlock().getType() == Material.LOG 
-				|| block.getLocation().subtract(0,0,1).getBlock().getType() == Material.LOG_2 
-				|| block.getLocation().subtract(0,0,1).getBlock().getType() == Material.LEAVES
-				|| block.getLocation().subtract(0,0,1).getBlock().getType() == Material.LEAVES_2)
-		{
-			if(S87Powers.canPlayerBuildAt(p, block.getLocation().subtract(0,0,1), block.getLocation().subtract(0,0,1).getBlock()))
-			{
-				block.getLocation().subtract(0,0,1).getBlock().breakNaturally();
-				blocksBroken++;
-				breakAdjacentBlocks(p,block.getLocation().subtract(0,0,1).getBlock());
-			}
-		}
-		//check +1x +1z
-		if(block.getLocation().add(1,0,1).getBlock().getType() == Material.LOG 
-				|| block.getLocation().add(1,0,1).getBlock().getType() == Material.LOG_2 
-				|| block.getLocation().add(1,0,1).getBlock().getType() == Material.LEAVES
-				|| block.getLocation().add(1,0,1).getBlock().getType() == Material.LEAVES_2)
-		{
-			if(S87Powers.canPlayerBuildAt(p, block.getLocation().subtract(0,0,1), block.getLocation().subtract(0,0,1).getBlock()))
-			{
-				block.getLocation().subtract(0,0,1).getBlock().breakNaturally();
-				blocksBroken++;
-				breakAdjacentBlocks(p,block.getLocation().subtract(0,0,1).getBlock());
-			}
-		}
-		//check +1x -1z
-		if(block.getLocation().add(1,0,-1).getBlock().getType() == Material.LOG 
-				|| block.getLocation().add(1,0,-1).getBlock().getType() == Material.LOG_2 
-				|| block.getLocation().add(1,0,-1).getBlock().getType() == Material.LEAVES
-				|| block.getLocation().add(1,0,-1).getBlock().getType() == Material.LEAVES_2)
-		{
-			if(S87Powers.canPlayerBuildAt(p, block.getLocation().add(1,0,-1), block.getLocation().add(1,0,-1).getBlock()))
-			{
-				block.getLocation().add(1,0,-1).getBlock().breakNaturally();
-				blocksBroken++;
-				breakAdjacentBlocks(p,block.getLocation().add(1,0,-1).getBlock());
-			}	
-		}
-		//check -1x +1z
-		if(block.getLocation().add(-1,0,1).getBlock().getType() == Material.LOG 
-				|| block.getLocation().add(-1,0,1).getBlock().getType() == Material.LOG_2 
-				|| block.getLocation().add(-1,0,1).getBlock().getType() == Material.LEAVES
-				|| block.getLocation().add(-1,0,1).getBlock().getType() == Material.LEAVES_2)
-		{
-			if(S87Powers.canPlayerBuildAt(p, block.getLocation().add(-1,0,1), block.getLocation().add(-1,0,1).getBlock()))
-			{
-				block.getLocation().add(-1,0,1).getBlock().breakNaturally();
-				blocksBroken++;
-				breakAdjacentBlocks(p,block.getLocation().add(-1,0,1).getBlock());
-			}
-		}
-		//check -1x -1z
-		if(block.getLocation().add(-1,0,-1).getBlock().getType() == Material.LOG 
-				|| block.getLocation().add(-1,0,-1).getBlock().getType() == Material.LOG_2 
-				|| block.getLocation().add(-1,0,-1).getBlock().getType() == Material.LEAVES
-				|| block.getLocation().add(-1,0,-1).getBlock().getType() == Material.LEAVES_2)
-		{
-			if(S87Powers.canPlayerBuildAt(p, block.getLocation().add(-1,0,-1), block.getLocation().add(-1,0,-1).getBlock()))
-			{
-				block.getLocation().add(-1,0,-1).getBlock().breakNaturally();
-				blocksBroken++;
-				breakAdjacentBlocks(p,block.getLocation().add(-1,0,-1).getBlock());
+				for(int z = -1; z < 2; z++)
+				{
+					if(!(x == 0 && y == 0 && z == 0))	//do not check the original block -- this would cause an infinite loop
+					{
+						if(block.getLocation().add(x,y,z).getBlock().getType() == Material.LOG 			//check if block is of type log or leaf
+								|| block.getLocation().add(x,y,z).getBlock().getType() == Material.LOG_2 
+								|| block.getLocation().add(x,y,z).getBlock().getType() == Material.LEAVES
+								|| block.getLocation().add(x,y,z).getBlock().getType() == Material.LEAVES_2)
+						if(S87Powers.canPlayerBuildAt(p, block.getLocation().add(x,y,z), block.getLocation().add(x,y,z).getBlock()))
+						{
+							block.getLocation().add(x,y,z).getBlock().breakNaturally();
+							blocksBroken++;
+							breakAdjacentBlocks(p,block.getLocation().add(x,y,z).getBlock());
+						}
+					}
+				}
 			}
 		}
 		//failed to find additional blocks to destroy, return
