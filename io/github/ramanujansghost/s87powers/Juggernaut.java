5bfcfa6190ba431757f22a0762b15675e5eae023
knightfallx66 <gcs.player@gmail.com>
2016-06-25 21:10:25 -0400

    Added Shell, Juggernaut

---
+++ io/github/ramanujansghost/s87powers/Juggernaut.java
@@ -0,0 +1,13 @@
+package io.github.ramanujansghost.s87powers;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class Juggernaut 
+{
+	public static void onRightClick(Player p) 
+	{
+			p.setMaxHealth(40);
+	}
+
+}
