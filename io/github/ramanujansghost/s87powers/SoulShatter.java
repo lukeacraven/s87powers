e8095e356bc6c2411057038da9f95b4311ec1f6d
knightfallx66 <gcs.player@gmail.com>
2016-07-01 09:13:05 -0400

    Soulshatter respects slipgates, properly destroys

--- io/github/ramanujansghost/s87powers/SoulShatter.java
+++ io/github/ramanujansghost/s87powers/SoulShatter.java
@@ -1,6 +1,11 @@
 package io.github.ramanujansghost.s87powers;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -14,7 +19,7 @@ import org.bukkit.potion.PotionEffectType;
 
 public class SoulShatter 
 {
-	public static void onRightClick(Player p)
+	public static void onRightClick(Player p) throws SQLException
 	{
 		System.out.println("Begin Shatter");
 		World s = p.getWorld();
@@ -37,25 +42,64 @@ public class SoulShatter
 	    	 p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 999999, 10));
 	    	 p.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 999999, 10));
 	      }
-	      
+	      //.setType(Material.AIR)
+	      ArrayList<Block> destroy = new ArrayList<Block>();
 	      for (int z = 0; z <= bsize; z++) {
 	            zpow = Math.pow(z, 2);
 	            for (int x = 0; x <= bsize; x++) {
 	                xpow = Math.pow(x, 2);
 	                for (int y = 0; y <= bsize; y++) {
 	                    if ((xpow + Math.pow(y, 2) + zpow) <= bpow) { 
-	                        s.getBlockAt(bx + x, by + y, bz + z).setType(Material.AIR);
-	                        s.getBlockAt(bx + x, by + y, bz - z).setType(Material.AIR);
-	                        s.getBlockAt(bx - x, by + y, bz + z).setType(Material.AIR);
-	                        s.getBlockAt(bx - x, by + y, bz - z).setType(Material.AIR);
-	                        s.getBlockAt(bx + x, by - y, bz + z).setType(Material.AIR);
-	                        s.getBlockAt(bx + x, by - y, bz - z).setType(Material.AIR);
-	                        s.getBlockAt(bx - x, by - y, bz + z).setType(Material.AIR);
-	                        s.getBlockAt(bx - x, by - y, bz - z).setType(Material.AIR);
+	                    	destroy.add(s.getBlockAt(bx + x, by + y, bz + z));
+	                    	destroy.add(s.getBlockAt(bx + x, by + y, bz - z));
+	                    	destroy.add(s.getBlockAt(bx - x, by + y, bz + z));
+	                    	destroy.add(s.getBlockAt(bx - x, by + y, bz - z));
+	                    	destroy.add(s.getBlockAt(bx + x, by - y, bz + z));
+	                    	destroy.add(s.getBlockAt(bx + x, by - y, bz - z));
+	                    	destroy.add(s.getBlockAt(bx - x, by - y, bz + z));
+	                    	destroy.add(s.getBlockAt(bx - x, by - y, bz - z));
 	                    }
 	                }
 	            }
 	        }
+	      for(Block b : destroy)
+	      {
+				if(b.getType() == Material.OBSIDIAN)
+				{
+					System.out.println("Is Obby");
+					
+					for(Iterator<Map.Entry<Block, Integer>> it = S87Powers.slipGateLocs.entrySet().iterator(); it.hasNext(); )
+					{
+					      Entry<Block, Integer> entry = it.next();
+							if(b.getLocation().distanceSquared(entry.getKey().getLocation()) < 16)
+							{
+								System.out.println("Is <16");
+								for(Block gateBlock : GateBuilder.checkShape(entry.getKey().getRelative(0, -1, 0), entry.getValue()))
+								{
+									if(b.equals(gateBlock))
+									{
+										System.out.println("Is Gate");
+										entry.getKey().setType(Material.AIR);
+										entry.getKey().getRelative(0,1,0).setType(Material.AIR);
+										GateBuilder.removeGateFromDB(entry.getKey(), entry.getValue());
+										it.remove();
+
+										b.setType(Material.AIR);
+									}
+								}
+							}
+							else
+							{
+								b.setType(Material.AIR);
+							}
+					}
+				}
+				else
+				{
+					b.setType(Material.AIR);
+				}
+	      }
+	      
 	      for(Entity e: p.getNearbyEntities(40, 40, 40))
 	      {
 	    	 if(e instanceof LivingEntity)
