5dfa9b397e6fdaf922f1773698a54ce2f68901d1
knightfallx66 <gcs.player@gmail.com>
2016-07-05 21:56:48 -0400

    Comments, and now has good entity targeting

--- io/github/ramanujansghost/s87powers/Ensnare.java
+++ io/github/ramanujansghost/s87powers/Ensnare.java
@@ -2,69 +2,65 @@ package io.github.ramanujansghost.s87powers;
 
 import java.util.ArrayList;
 import java.util.Set;
-
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class Ensnare{
 	
-	//Constructor
-	public Ensnare()
-	{
-
-	}
 	
 	private static final ItemStack redstone = new ItemStack(Material.REDSTONE,1);
 	private static final ItemStack string = new ItemStack(Material.STRING,1);
 	
-	//This is not ready :p
-	//Creates 1 web at target location
+	//Creates web cross at block or on a target
 	public void deployWebbing(Player p)
 	{
     	Set<Material> st = null;
     	Block target = p.getTargetBlock(st, 8);
-
-		if(target.getType() != Material.AIR){			
+    	LivingEntity targetEntity = PlayerHelper.getTarget(p, 8);
     	
-			if(InventoryHelper.checkForReagents(p, string, 1) && InventoryHelper.checkForReagents(p, redstone, 1))
+		//Targeting an entity
+		if(targetEntity != null)
+		{			
+			if(InventoryHelper.checkForReagents(p, string, 6) && InventoryHelper.checkForReagents(p, redstone, 1))
+			{
+				InventoryHelper.removeReagents(p, string, 6);
+				InventoryHelper.removeReagents(p, redstone, 2);
+				ArrayList<Block> line = new ArrayList<Block>();
+				line.add(targetEntity.getLocation().getBlock());
+				line.add(targetEntity.getLocation().getBlock().getRelative(0, 1, 0));
+				line.add(targetEntity.getLocation().getBlock().getRelative(1, 0, 0));
+				line.add(targetEntity.getLocation().getBlock().getRelative(-1, 0, 0));
+				line.add(targetEntity.getLocation().getBlock().getRelative(0, 0, 1));
+				line.add(targetEntity.getLocation().getBlock().getRelative(0, 0, -1));
+				BlockHelper.placeInEmpty(p, line, Material.WEB);
+			}
+			else
+		    	p.sendMessage("You do not have the necessary reagents generate web!  Requires 6 string and 2 redstone.");
+		}
+    	//Targeting a block
+    	else if(target.getType() != Material.AIR)
+		{			
+    	
+			if(InventoryHelper.checkForReagents(p, string, 6) && InventoryHelper.checkForReagents(p, redstone, 1))
 			{
 				Block close = BlockHelper.closestToPlayer(BlockHelper.findAir(target), p);
 				if(close != null)
 				{
-					InventoryHelper.removeReagents(p, string, 2);
+					InventoryHelper.removeReagents(p, string, 6);
 					InventoryHelper.removeReagents(p, redstone, 2);
 					BlockHelper.placeInEmpty(p, BlockHelper.getCrossOnFace(close, target.getFace(BlockHelper.closestToPlayer(BlockHelper.findAir(target), p))), Material.WEB);
 				}
 				
 			}
 			else
-		    	p.sendMessage("You do not have the necessary reagents generate web!  Requires 1 string and 1 redstone.");
+		    	p.sendMessage("You do not have the necessary reagents generate web!  Requires 6 string and 2 redstone.");
 		}
-		else
-		{
-			if(InventoryHelper.checkForReagents(p, string, 1) && InventoryHelper.checkForReagents(p, redstone, 1))
-			{
-				InventoryHelper.removeReagents(p, string, 2);
-				InventoryHelper.removeReagents(p, redstone, 2);
-				ArrayList<Block> line = new ArrayList<Block>();
-				//Not sure this is best, but seems to work alright
-				line.add(p.getTargetBlock(st,1));
-				line.add(p.getTargetBlock(st,2));
-				line.add(p.getTargetBlock(st,3));
-				line.add(p.getTargetBlock(st,4));
-				
-				//BlockHelper.placeInEmpty(BlockHelper.getLine(tempWorld, p.getEyeLocation(), 4, PlayerHelper.getPlayerFacing(p)), Material.WEB);
-				BlockHelper.placeInEmpty(p, line, Material.WEB);
-				
-			}
-			else
-		    	p.sendMessage("You do not have the necessary reagents generate web!  Requires 1 string and 1 redstone.");
 			
-		}
-
 	}
+
 	
 
 }
