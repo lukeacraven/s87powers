b41a149ebf115862513a57a368b5c30a7b0c768b
knightfallx66 <gcs.player@gmail.com>
2016-07-05 22:04:49 -0400

    Changed math sorta, added cost

--- io/github/ramanujansghost/s87powers/Push.java
+++ io/github/ramanujansghost/s87powers/Push.java
@@ -1,35 +1,38 @@
 package io.github.ramanujansghost.s87powers;
 
-import org.bukkit.Material;
-import org.bukkit.block.BlockFace;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
 public class Push 
 {
+	//Feels okay, may need twerking
 	public static void onRightClick(Player p) 
 	{
-		Vector push = p.getLocation().getDirection();
-		//Pull
-		if(p.isSneaking())
+		LivingEntity target = PlayerHelper.getTarget(p, 10);
+		if(target != null)
 		{
-			LivingEntity target = PlayerHelper.getTarget(p, 12);
-			if(target != null)
+			if(GemHelper.cast(p.getInventory(), 7))
 			{
-				target.setVelocity(new Vector(push.getX()*-4, (push.getY()+.4)*-2, push.getZ()*-4));
+				Vector push = p.getLocation().getDirection();
+				//Pull
+				if(p.isSneaking())
+				{
+					if(target != null)
+					{
+						target.setVelocity(new Vector(push.getX()*(-4), (push.getY()+.1)*-2, push.getZ()*-4));
+					}
+				}
+				//Push
+				else
+				{
+					if(target != null)
+					{
+						target.setVelocity(new Vector(push.getX()*4, (push.getY()+.1)*2, push.getZ()*4));
+					}
+				}
 			}
 		}
-		//Push
-		else
-		{
-			LivingEntity target = PlayerHelper.getTarget(p, 8);
-			if(target != null)
-			{
-				target.setVelocity(new Vector(push.getX()*4, (push.getY()+.4)*2, push.getZ()*4));
-			}
-		}
-
 	}
 
 }
