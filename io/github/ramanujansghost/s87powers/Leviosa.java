4b99a67bddecf856ee27e5405290d8c6d5595ffb
knightfallx66 <gcs.player@gmail.com>
2016-07-05 22:01:56 -0400

    Added cost

--- io/github/ramanujansghost/s87powers/Leviosa.java
+++ io/github/ramanujansghost/s87powers/Leviosa.java
@@ -7,17 +7,18 @@ import org.bukkit.util.Vector;
 //Wingardium Leviosaaa
 public class Leviosa
 {
+	//Levitate a target entity
 	public static void onRightClick(Player p) 
 	{
 		//UP UP and AWAY
+		//8 Block range
 		LivingEntity target = PlayerHelper.getTarget(p, 8);
-		if(target != null)
+		if(target != null && GemHelper.cast(p.getInventory(), 1))
 		{
 			Vector up = target.getVelocity();
 			target.setVelocity(new Vector(up.getX(), (.3), up.getZ()));
 			target.setFallDistance(0);
-			LivingEntity j = null;
 		}
-		}
+	}
 
 }
