6324954d666d004bbcc7b9b8511df1b539c8416a
knightfallx66 <gcs.player@gmail.com>
2016-07-05 22:06:25 -0400

    Added cost

--- io/github/ramanujansghost/s87powers/Translocation.java
+++ io/github/ramanujansghost/s87powers/Translocation.java
@@ -3,20 +3,23 @@ package io.github.ramanujansghost.s87powers;
 import org.bukkit.Location;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.util.Vector;
 
 public class Translocation 
 {
+	//Swap the locations of player with target entity
 	public static void onRightClick(Player p) 
 	{
 		//SWAP!
 		LivingEntity target = PlayerHelper.getTarget(p, 30);
 		if(target != null)
 		{
-			Location me = p.getLocation();
-			Location you = target.getLocation();
-			p.teleport(you);
-			target.teleport(me);
+			if(GemHelper.cast(p.getInventory(),(int) target.getLocation().distance(p.getLocation())/2))
+			{
+				Location me = p.getLocation();
+				Location you = target.getLocation();
+				p.teleport(you.setDirection(p.getLocation().getDirection()));
+				target.teleport(me.setDirection(target.getLocation().getDirection()));
+			}
 		}
 		}
 
