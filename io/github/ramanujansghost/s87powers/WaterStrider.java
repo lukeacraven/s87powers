58871edef84a9476fee3d993558f88c6d0263bfb
knightfallx66 <gcs.player@gmail.com>
2016-07-05 22:06:51 -0400

    Allows striding in water

---
+++ io/github/ramanujansghost/s87powers/WaterStrider.java
@@ -0,0 +1,21 @@
+package io.github.ramanujansghost.s87powers;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.util.Vector;
+
+public class WaterStrider 
+{
+	//Press and release shift to jet forward in water
+	public static void onShift(PlayerToggleSneakEvent e)
+	{
+		Player p = e.getPlayer();
+		if(p.isSneaking() && (p.getLocation().getBlock().getType() == Material.WATER || p.getLocation().getBlock().getType() == Material.STATIONARY_WATER))
+		{
+			Vector vel = p.getLocation().getDirection();
+			p.setVelocity(new Vector(vel.getX()*2,vel.getY()*1.3,vel.getZ()*2));
+		}
+	}
+
+}
