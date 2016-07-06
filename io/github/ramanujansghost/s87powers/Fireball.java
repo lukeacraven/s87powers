6a5c990df9f428115dfc2bc1e9f19d53747be811
knightfallx66 <gcs.player@gmail.com>
2016-07-05 21:57:03 -0400

    Added cost

--- io/github/ramanujansghost/s87powers/Fireball.java
+++ io/github/ramanujansghost/s87powers/Fireball.java
@@ -1,22 +1,21 @@
 package io.github.ramanujansghost.s87powers;
 
-import org.bukkit.Material;
-import org.bukkit.block.BlockFace;
 import org.bukkit.entity.LargeFireball;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
-import org.bukkit.entity.SmallFireball;
 import org.bukkit.util.Vector;
 
+//Pretty simple, just creates a fire-ball
 public class Fireball 
 {
-	@SuppressWarnings("null")
 	public static void onRightClick(Player p) 
 	{
-		Vector vel = p.getLocation().getDirection();
-		Projectile ball = p.launchProjectile(LargeFireball.class);
-		ball.setVelocity(vel);
-
+		if(GemHelper.cast(p.getInventory(), 10))
+		{
+			Vector vel = p.getLocation().getDirection();
+			Projectile ball = p.launchProjectile(LargeFireball.class);
+			ball.setVelocity(vel);
+		}
 		
 	}
 
