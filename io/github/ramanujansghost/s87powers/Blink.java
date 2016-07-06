29540be922ff475edf5818a923e616cfec8ce889
knightfallx66 <gcs.player@gmail.com>
2016-07-05 21:55:08 -0400

    Minor improvements, comments

--- io/github/ramanujansghost/s87powers/Blink.java
+++ io/github/ramanujansghost/s87powers/Blink.java
@@ -1,56 +1,46 @@
 package io.github.ramanujansghost.s87powers;
 
 import java.util.Set;
-
-import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.block.Block;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.util.Vector;
 
-//182
-public class Blink 
+
+public class Blink //182
 {
 	public static void onRightClick(Player p) 
 	{
-		//if(canBlink(p))
-		{
-			System.out.println("Blink Start");
-			//Set<Material> st = null;
-			System.out.println("Target");
-	    	Block target = p.getTargetBlock((Set<Material>)null, 15);
-	    	if(target.getType() != Material.AIR)
-	    	{
-	    		System.out.println("Casting");
-	    		if(GemHelper.cast(p.getInventory(),(int) target.getLocation().distance(p.getLocation())/2))
-		    	{
-	    			System.out.println("Casting");
-	    			Block close = BlockHelper.closestToPlayer(BlockHelper.findAir(target), p);
-	    			System.out.println("Found closest");
-	    			if(p.isSneaking())
-	    			{
-	    				if(target.getLocation().add(0,1,0).getBlock().getType()== Material.AIR)
-	    				{
-	    					p.teleport(target.getLocation().setDirection(p.getLocation().getDirection()).add(0,1,0));
-	    					//S87Powers.timeTillBlinkAgain.put(p.getUniqueId(), System.currentTimeMillis()+ 1000);
-	    				}
-	    			}
-	    			else if(close.getLocation().add(0,1,0).getBlock().getType() == Material.AIR)
-	    			{
-	    				p.teleport(close.getLocation().setDirection(p.getLocation().getDirection()));
-	    				//S87Powers.timeTillBlinkAgain.put(p.getUniqueId(), System.currentTimeMillis()+ 1000);
-	    			}
-	    			System.out.println("Teleport");
-	    			
-		    	}	
-	    	}
-		}
+		//Get target
+    	Block target = p.getTargetBlock((Set<Material>)null, 15);
 
-		}
-	
+    	if(target.getType() != Material.AIR)
+    	{
+        	//Valid target
+			Block close = BlockHelper.closestToPlayer(BlockHelper.findAir(target), p);
+			//If shift, blink to top of block
+			if(p.isSneaking())
+			{
+				//Check for free target location
+				if(target.getLocation().add(0,1,0).getBlock().getType()== Material.AIR)
+				{
+					//Extract power/do the thing
+					if(GemHelper.cast(p.getInventory(),(int) target.getLocation().distance(p.getLocation())/2))
+					{
+						p.teleport(target.getLocation().setDirection(p.getLocation().getDirection()).add(0,1,0));
+					}
+				}
+			}
+			//Check for free target location
+			else if(close.getLocation().add(0,1,0).getBlock().getType() == Material.AIR)
+			{
+				//Extract power/do the thing
+				if(GemHelper.cast(p.getInventory(),(int) target.getLocation().distance(p.getLocation())/2))
+				{
+					p.teleport(close.getLocation().setDirection(p.getLocation().getDirection()));
+				}
+			}    				
+    	}
 
+	}
 
 }
