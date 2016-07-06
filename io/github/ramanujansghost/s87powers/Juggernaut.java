7ba2663fa243808c0600b45de4b55a337c27e3e4
knightfallx66 <gcs.player@gmail.com>
2016-07-05 21:59:45 -0400

    Added comment

--- io/github/ramanujansghost/s87powers/Juggernaut.java
+++ io/github/ramanujansghost/s87powers/Juggernaut.java
@@ -1,8 +1,8 @@
 package io.github.ramanujansghost.s87powers;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+//Not ready atm, needs bool in database or something. Although, I think it lasts beyond reloads.
 public class Juggernaut 
 {
 	public static void onRightClick(Player p) 
