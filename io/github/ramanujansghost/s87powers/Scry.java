3496aa7df33909c5eef017cc2751b4e36da6ddc0
knightfallx66 <gcs.player@gmail.com>
2016-07-05 22:05:16 -0400

    Cleanup, not ready

--- io/github/ramanujansghost/s87powers/Scry.java
+++ io/github/ramanujansghost/s87powers/Scry.java
@@ -4,9 +4,10 @@ import org.bukkit.entity.Player;
 
 public class Scry
 {
+	//Not ready
 	public static void look(Player p)
 	{
-		System.out.println("Scrying");
+		//System.out.println("Scrying");
 		//p.setGameMode(GameMode.SPECTATOR);
 		//p.setSpectatorTarget(p.getNearbyEntities(100, 100, 100).get(1));
 	}
