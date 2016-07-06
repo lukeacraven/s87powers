8d2803ccdd9a9168008c8ae99fa2c1eb66ac6adf
knightfallx66 <gcs.player@gmail.com>
2016-07-05 22:01:31 -0400

    Now cost magic or whatever

--- io/github/ramanujansghost/s87powers/Letta.java
+++ io/github/ramanujansghost/s87powers/Letta.java
@@ -1,22 +1,22 @@
 package io.github.ramanujansghost.s87powers;
 
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class Letta
 {
-	//Constructor
-	public Letta()
-	{
-
-	}
-
 	//When hit, ignore damage, disappear arrow
+	//You'd better be handing this a player :P
 	public void onArrowHitPlayer(EntityDamageByEntityEvent event, Entity damagedEntity)
 	{
-		event.setCancelled(true);
-		event.getDamager().remove();
-		damagedEntity.sendMessage("Your swift reflexes let you evade the arrow!");		
+		//Block if you have the magic/mana/lifeforce/whatever
+		if(GemHelper.cast(((Player)damagedEntity).getInventory(), 14))
+		{
+			event.setCancelled(true);
+			event.getDamager().remove();
+			damagedEntity.sendMessage("Your magic blocked an incoming arrow!");		
+		}
 	}
 	
 }
