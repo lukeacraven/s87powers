5e1b27e798195f01754f110cd14f93f0e5dc7c68
knightfallx66 <gcs.player@gmail.com>
2016-07-08 19:08:54 -0400

    No longer static

--- io/github/ramanujansghost/s87powers/Scry.java
+++ io/github/ramanujansghost/s87powers/Scry.java
@@ -5,9 +5,9 @@ import org.bukkit.entity.Player;
 public class Scry
 {
 	//Not ready
-	public static void look(Player p)
+	public void look(Player p)
 	{
-		//System.out.println("Scrying");
+		//System.out.println("Scrying"); //Crying
 		//p.setGameMode(GameMode.SPECTATOR);
 		//p.setSpectatorTarget(p.getNearbyEntities(100, 100, 100).get(1));
 	}
