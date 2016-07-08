9e8fb005889cdb895918e55e54119ecca92e0bf8
knightfallx66 <gcs.player@gmail.com>
2016-07-08 19:03:43 -0400

    No longer static

--- io/github/ramanujansghost/s87powers/Juggernaut.java
+++ io/github/ramanujansghost/s87powers/Juggernaut.java
@@ -5,7 +5,7 @@ import org.bukkit.entity.Player;
 //Not ready atm, needs bool in database or something. Although, I think it lasts beyond reloads.
 public class Juggernaut 
 {
-	public static void onRightClick(Player p) 
+	public void onRightClick(Player p) 
 	{
 			p.setMaxHealth(40);
 	}
