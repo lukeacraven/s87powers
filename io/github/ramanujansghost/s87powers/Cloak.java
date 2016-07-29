fcba366d047031ac4d8f4a56f0ed0a014f97249e
knightfallx66 <gcs.player@gmail.com>
2016-07-29 17:49:23 -0400

    Actually works now

--- io/github/ramanujansghost/s87powers/Cloak.java
+++ io/github/ramanujansghost/s87powers/Cloak.java
@@ -1,6 +1,7 @@
 package io.github.ramanujansghost.s87powers;
 
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 public class Cloak 
@@ -8,10 +9,33 @@ public class Cloak
 	//Not Ready for Release... Need to Stealthing method
 	public void onRightClick(Player p) 
 	{
-			for(Player onP: Bukkit.getOnlinePlayers())
+		System.out.println("Cloaking!");
+			for(OfflinePlayer onP: Bukkit.getOfflinePlayers())
 			{
-				onP.hidePlayer(p);
+				if(onP.getPlayer() != null)
+					onP.getPlayer().hidePlayer(p);
 			}
+//			for(Player onP: Bukkit.getOnlinePlayers())
+//			{
+//				onP.hidePlayer(p);
+//			}
+			p.sendMessage("You've vanished!");
+			S87Powers.allPlayers.get(p.getUniqueId()).addStatus("Cloaked");
+	}
+	public void reveal(Player p) 
+	{
+		System.out.println("DeCloaking!");
+		for(OfflinePlayer onP: Bukkit.getOfflinePlayers())
+		{
+			if(onP.getPlayer() != null)
+				onP.getPlayer().showPlayer(p);
+		}
+		p.sendMessage("You've been revealed!");
+		S87Powers.allPlayers.get(p.getUniqueId()).removeStatus("Cloaked");
+//		for(Player onP: Bukkit.getOnlinePlayers())
+//		{
+//			onP.showPlayer(p);
+//		}
 	}
 
 }
