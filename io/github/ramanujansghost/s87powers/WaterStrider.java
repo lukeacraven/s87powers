2f3c843e4758c13e15b5c4bb217e21067d9d869d
knightfallx66 <gcs.player@gmail.com>
2016-07-31 18:54:20 -0400

    Respects armor

--- io/github/ramanujansghost/s87powers/WaterStrider.java
+++ io/github/ramanujansghost/s87powers/WaterStrider.java
@@ -11,10 +11,17 @@ public class WaterStrider
 	public void onShift(PlayerToggleSneakEvent e)
 	{
 		Player p = e.getPlayer();
-		if(p.isSneaking() && (p.getLocation().getBlock().getType() == Material.WATER || p.getLocation().getBlock().getType() == Material.STATIONARY_WATER))
+		if(PlayerHelper.getArmorCount(p) == 0)
 		{
-			Vector vel = p.getLocation().getDirection();
-			p.setVelocity(new Vector(vel.getX()*2,vel.getY()*1.3,vel.getZ()*2));
+			if(p.isSneaking() && (p.getLocation().getBlock().getType() == Material.WATER || p.getLocation().getBlock().getType() == Material.STATIONARY_WATER))
+			{
+				Vector vel = p.getLocation().getDirection();
+				p.setVelocity(new Vector(vel.getX()*2,vel.getY()*1.3,vel.getZ()*2));
+			}
+		}
+		else
+		{
+			p.sendMessage("Your armor is weighing you down too much");
 		}
 	}
 
