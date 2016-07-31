ee02666f9f554e8f8a4ffadcbf8c0ce47206d0a4
knightfallx66 <gcs.player@gmail.com>
2016-07-31 18:53:41 -0400

    Lots of fixes, now working

--- io/github/ramanujansghost/s87powers/GrowthAlchemy.java
+++ io/github/ramanujansghost/s87powers/GrowthAlchemy.java
@@ -5,7 +5,9 @@ import java.util.Random;
 import java.util.Set;
 
 import org.bukkit.CropState;
+import org.bukkit.GrassSpecies;
 import org.bukkit.Material;
+import org.bukkit.TreeSpecies;
 import org.bukkit.TreeType;
 import org.bukkit.World;
 import org.bukkit.block.Block;
@@ -13,12 +15,17 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.material.MaterialData;
+import org.bukkit.material.Crops;
+import org.bukkit.material.Leaves;
+import org.bukkit.material.LongGrass;
+import org.bukkit.material.Tree;
+import org.bukkit.material.Vine;
+
 
 public class GrowthAlchemy 
 {
 	private final ItemStack redstone = new ItemStack(Material.REDSTONE,1);
-	@SuppressWarnings("deprecation")
+
 	public void onRightClick(Player p)
 	{
 		Set<Material> st = null;
@@ -33,8 +40,12 @@ public class GrowthAlchemy
 					InventoryHelper.removeReagents(p, redstone, 1);
 					target.setType(Material.GRASS);
 				}
+				else
+				{
+					p.sendMessage("You cannot target claimed land");
+				}
 			}
-			else if(type == Material.LEAVES || type == Material.LEAVES_2)
+			else if(type == Material.LEAVES || type == Material.LEAVES_2 || type == Material.LOG || type == Material.LOG_2)
 			{
 	
 				if(InventoryHelper.checkForReagents(p, redstone, 4))
@@ -47,7 +58,29 @@ public class GrowthAlchemy
 					grow.add(target.getRelative(BlockFace.EAST));
 					grow.add(target.getRelative(BlockFace.WEST));
 					InventoryHelper.removeReagents(p, redstone, 4);
-					BlockHelper.placeInEmpty(p, grow, type, true);
+					BlockHelper.placeInEmpty(p, grow, Material.LEAVES, true);
+					TreeSpecies ts;
+					if(type == Material.LOG || type == Material.LOG_2)
+					{
+						Tree treLea = (Tree) target.getState().getData();
+						ts = treLea.getSpecies();
+					}
+					else
+					{
+						Leaves tarLea = (Leaves) target.getState().getData();
+						ts = tarLea.getSpecies();
+					}
+					for(Block b : grow)
+					{
+						if(b.getType() == Material.LEAVES || b.getType() == Material.LEAVES_2)
+						{
+							BlockState bs = b.getState();
+							Leaves lg = (Leaves) bs.getData();
+							lg.setDecayable(false);
+							lg.setSpecies(ts);
+							bs.update();
+						}
+					}
 				}
 			}
 			else if(type == Material.GRASS)
@@ -61,6 +94,16 @@ public class GrowthAlchemy
 					grow.add(target.getRelative(-1,1,0));
 					grow.add(target.getRelative(0,1,-1));
 					BlockHelper.placeInEmpty(p, grow, Material.LONG_GRASS, true);
+					for(Block b : grow)
+					{
+						if(b.getType() == Material.LONG_GRASS)
+						{
+							BlockState bs = b.getState();
+							LongGrass lg = (LongGrass) bs.getData();
+							lg.setSpecies(GrassSpecies.NORMAL);
+							bs.update();
+						}
+					}
 					InventoryHelper.removeReagents(p, redstone, 3);
 				}
 			}
@@ -108,14 +151,35 @@ public class GrowthAlchemy
 				}
 				
 			}
-			else if(type == Material.CROPS)
+			else if(type == Material.CARROT || type == Material.CROPS || type == Material.SEEDS || type == Material.POTATO || type == Material.BEETROOT_BLOCK)
 			{
 				if(InventoryHelper.checkForReagents(p, redstone, 3))
 				{
-					target.setData((byte) 7);
+					BlockState bs = target.getState();
+					Crops crop = (Crops)bs.getData();
+					crop.setState(CropState.RIPE);
+					bs.update();
 					InventoryHelper.removeReagents(p, redstone, 3);
 				}
 
+			}
+			else if(type == Material.MELON_STEM)
+			{
+				target.setType(Material.MELON_BLOCK);
+				InventoryHelper.removeReagents(p, redstone, 3);
+			}
+			else if(type == Material.PUMPKIN_STEM)
+			{
+				target.setType(Material.PUMPKIN);
+				InventoryHelper.removeReagents(p, redstone, 3);
+			}
+			else if(type == Material.SUGAR_CANE_BLOCK)
+			{
+				if(target.getRelative(BlockFace.UP).getType() == Material.AIR)
+				{
+					target.getRelative(BlockFace.UP).setType(Material.SUGAR_CANE_BLOCK);
+					InventoryHelper.removeReagents(p, redstone, 3);
+				}
 			}
 		}
 		else if(S87Powers.canPlayerBuildAt(p, target.getLocation(), target) && InventoryHelper.checkForReagents(p, redstone, 3))
@@ -126,6 +190,17 @@ public class GrowthAlchemy
 			grow.add(target.getRelative(0,0,1));
 			grow.add(target.getRelative(0,0,-1));
 			BlockHelper.placeInEmpty(p, grow, Material.VINE, true);
+
+			for(Block g : grow)
+			{
+				if(g.getType() == Material.VINE)
+				{
+					BlockState bs = g.getState();
+					Vine v = (Vine)bs.getData();
+					v.putOnFace(g.getFace(target));	
+					bs.update();
+				}
+			}
 			InventoryHelper.removeReagents(p, redstone, 3);
 		}
 
