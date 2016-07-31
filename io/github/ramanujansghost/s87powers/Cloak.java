2ff049361ef381e9f773d3ba9bb04b015eaffa26
knightfallx66 <gcs.player@gmail.com>
2016-07-31 18:53:21 -0400

    Works well now

--- io/github/ramanujansghost/s87powers/Cloak.java
+++ io/github/ramanujansghost/s87powers/Cloak.java
@@ -6,36 +6,34 @@ import org.bukkit.entity.Player;
 
 public class Cloak 
 {
-	//Not Ready for Release... Need to Stealthing method
 	public void onRightClick(Player p) 
 	{
-		System.out.println("Cloaking!");
+		GemHelper gh = new GemHelper();
+		if(gh.cast(p.getInventory(), 50))
+		{
 			for(OfflinePlayer onP: Bukkit.getOfflinePlayers())
 			{
 				if(onP.getPlayer() != null)
 					onP.getPlayer().hidePlayer(p);
 			}
-//			for(Player onP: Bukkit.getOnlinePlayers())
-//			{
-//				onP.hidePlayer(p);
-//			}
+
 			p.sendMessage("You've vanished!");
 			S87Powers.allPlayers.get(p.getUniqueId()).addStatus("Cloaked");
+		}
 	}
+	
 	public void reveal(Player p) 
 	{
-		System.out.println("DeCloaking!");
 		for(OfflinePlayer onP: Bukkit.getOfflinePlayers())
 		{
 			if(onP.getPlayer() != null)
 				onP.getPlayer().showPlayer(p);
 		}
-		p.sendMessage("You've been revealed!");
-		S87Powers.allPlayers.get(p.getUniqueId()).removeStatus("Cloaked");
-//		for(Player onP: Bukkit.getOnlinePlayers())
-//		{
-//			onP.showPlayer(p);
-//		}
+		if(S87Powers.allPlayers.get(p.getUniqueId()).getStati().contains("Cloaked"))
+		{
+			p.sendMessage("You've been revealed!");
+			S87Powers.allPlayers.get(p.getUniqueId()).removeStatus("Cloaked");
+		}
 	}
 
 }
